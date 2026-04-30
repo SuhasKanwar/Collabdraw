688d6c8bff1a13ba20daf723770d0d55c5b2a88b
Suhas Kanwar <suhas.kanwar@gmail.com>
2026-05-01 02:13:28 +0530

    fixed the api call domain prob

--- apps/collabdraw-frontend/src/config.ts
+++ apps/collabdraw-frontend/src/config.ts
@@ -1,2 +1,7 @@
-export const HTTP_BACKEND_URL = process.env.NEXT_PUBLIC_HTTP_BACKEND_URL || "http://localhost:9090";
-export const WS_BACKEND_URL = process.env.NEXT_PUBLIC_WS_BACKEND_URL || "ws://localhost:8080";
+const isBrowser = typeof window !== "undefined";
+
+export const HTTP_BACKEND_URL = process.env.NEXT_PUBLIC_HTTP_BACKEND_URL 
+  || (isBrowser ? `http://${window.location.hostname}:9090` : "http://localhost:9090");
+
+export const WS_BACKEND_URL = process.env.NEXT_PUBLIC_WS_BACKEND_URL 
+  || (isBrowser ? `ws://${window.location.hostname}:8080` : "ws://localhost:8080");
