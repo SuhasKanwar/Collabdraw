6f3aa9b600f479eff1310926cda64edee69834d2
Suhas Kanwar <suhas.kanwar@gmail.com>
2026-04-30 23:39:54 +0530

    patched the prisma problem

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,5 +1,11 @@
 import { PrismaClient } from "@prisma/client";
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
 
 const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
 
-export const prismaClient = globalForPrisma.prisma ?? (globalForPrisma.prisma = new PrismaClient());
+const connectionString = process.env.DATABASE_URL;
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+
+export const prismaClient = globalForPrisma.prisma ?? (globalForPrisma.prisma = new PrismaClient({ adapter }));
